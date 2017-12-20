ea21109d28281d3033245ecd818f5030b691fbf4
Hernán Aguilera <haguilera@openmailbox.org>
2017-12-20 08:19:15 -0600

    vuex and store configuration incorporated

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    loading: false
+  },
+  mutations: {
+    loading: (state) => {
+      state.loading = true
+    },
+    loaded: (state) => {
+      state.loading = false
+    }
+  },
+  getters: {}
+});
