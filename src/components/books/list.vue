2292de1ef1e5d09c8d5ee224b08841f9b1caa6f5
Hernán Aguilera <haguilera@openmailbox.org>
2017-12-20 09:15:24 -0600

    books list componen incorporated

---
+++ src/components/books/list.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Autor</th>
+          <th>Categoria</th>
+          <th>isbn</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in books" :key="book.id">
+          <td>{{book.name}}</td>
+          <td>{{book.author}}</td>
+          <td>{{book.genre}}</td>
+          <td>{{book.isbn}}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import Book from '@/utils/resources/Book'
+
+export default {
+  name: 'books_list',
+  data: () => {
+    return {
+      books: []
+    }
+  },
+  methods: {
+    load () {
+      let promise = Book.query()
+      promise.then((response) => {
+        console.log(response.data)
+        this.books = response.data
+      }, (error) => {
+        console.error(error)
+      })
+    }
+  },
+  created () {
+    this.load()
+  }
+}
+</script>
